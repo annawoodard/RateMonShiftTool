67cee2e742ddafb37d91e3f317b64734bd50eff0
Andrew Brinkerhoff <andrew.wilson.brinkerhoff@cern.chandrewbrink@gmail.com>
2012-02-23 16:57:27 +0000

    Andrew's setup.sh - CMSSW_5_x_x

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+export SCRAM_ARCH=slc5_amd64_gcc461
 pushd /nfshome0/cmssw
 source cmsset_default.sh
-cd slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_2/
+##cd slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_2/
+##cd slc5_amd64_gcc434/cms/cmssw/CMSSW_4_4_2/
+cd slc5_amd64_gcc461/cms/cmssw/CMSSW_5_0_1/
 cmsenv
 popd
+source /nfshome0/abrinke1/UserCode/Andrew/setupCVS.sh
