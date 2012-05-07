bfdf89f80ecb6784947c8d12e368c41480956175
Cory Fantasia <Cory.Fantasia@cern.chcfantasia@gmail.com>
2012-05-07 18:38:40 +0000

    safeguards

--- certifyRuns.sh
+++ certifyRuns.sh
@@ -4,10 +4,10 @@
 runList=runList.txt
 TriggerList=monitorlist_Apr_Core_2012.list
 firstFitRun=190782
-lastFitRun=191276
+lastFitRun=191859 #191276 #191859
 fitFile=Fits/2012/Fit_HLT_NoV_10LS_Run${firstFitRun}to${lastFitRun}.pkl
 fitRootFile=HLT_10LS_delivered_vs_rate_Run${firstFitRun}-${lastFitRun}.root
-JSONFile=Cert_190456-191276_8TeV_PromptReco_Collisions12_JSON.txt
+JSONFile=Cert_190456-191859_8TeV_PromptReco_Collisions12_JSON.txt
 
 if [ $# -ge 1 ]; then
     runList=${1}
@@ -37,18 +37,31 @@ echo Fit files are $fitFile $fitRootFile
 echo JSON file is $JSONFile
 
 #if fit file doesn't exist, make it!!!
-if [ ! -f $fitFile ]; then
+if [[ ( ! -f $fitFile ) || ( ! -f $fitRootFile ) ]]; then
     echo Fit file do not exist, creating
-    ./DatabaseRatePredictor.py --makeFits --TriggerList=$TriggerList --linear --NoVersion --json=$JSONFile ${firstFitRun}-${lastFitRun}
+    ./DatabaseRatePredictor.py --makeFits --TriggerList=$TriggerList --Beam --NoVersion --json=$JSONFile ${firstFitRun}-${lastFitRun}
     echo Done making fit file.
 fi
 
 for run in `cat $runList` 
   do
+  if [ -f HLT_1LS_ls_vs_rawrate_Run${run}-${run}.pdf ]; then
+      echo Skipping because .pdf exists for run $run
+      continue
+  fi
+
   echo Producing Plots for run $run
-  ./DatabaseRatePredictor.py --Beam --secondary --TriggerList=${TriggerList} --fitFile=${fitFile} ${run}
+  ./DatabaseRatePredictor.py --Beam --secondary --TriggerList=${TriggerList} --fitFile=${fitFile} ${run}  
+  if [ ! -f HLT_1LS_ls_vs_rawrate_Run${run}-${run}.root ]; then
+      echo -e "\n\tNo output root fit file for run $run, was the collisions key used?\n"
+      continue
+  fi
+
   root -b -l -q 'dumpToPDF.C("HLT_1LS_ls_vs_rawrate_Run'${run}'-'${run}'.root", "'${fitRootFile}'")'  
-  
+  if [ $? -ne 0 ]; then
+      echo Return value not 0!!!! Quitting...
+      exit 1
+  fi
 done
 
 echo Done.
