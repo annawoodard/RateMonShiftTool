e3c0ec909f5492848915be2f0769f3cfc913b9ab
Grant Edward Christopher <grant.christopher@cern.chgrant_christopher@brown.edu>
2012-06-27 09:02:25 +0000

    up date to set.sh -setting environment script

--- set.sh
+++ set.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-export SCRAM_ARCH=slc5_amd64_gcc461
-pushd /nfshome0/cmssw
+export SCRAM_ARCH=slc5_amd64_gcc462
+pushd /nfshome0/cmssw2
 source cmsset_default.sh
+cd /nfshome0/cmssw2/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_4/src
 ##cd slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_2/
-##cd slc5_amd64_gcc434/cms/cmssw/CMSSW_4_4_2/
-cd slc5_amd64_gcc461/cms/cmssw/CMSSW_5_0_1/
+#cd slc5_amd64_gcc434/cms/cmssw/CMSSW_4_4_2/
+##cd slc5_amd64_gcc461/cms/cmssw/CMSSW_5_0_1/
 cmsenv
 popd
 #source /nfshome0/abrinke1/UserCode/Andrew/setupCVS.sh
