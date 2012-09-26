8fea27c9a72a0811c249389d1ce08576a13035a1
annawoodard <awoodard@nd.edu>
2012-09-26 12:37:02 +0000

    Fixed indent bug, made stripversion also strip whitespace padding, fixed compatibility issues with new version of RateMonitor

--- MenuCompare.py
+++ MenuCompare.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+import string
 import subprocess
 import getopt
 import pickle
@@ -85,11 +86,11 @@ def main():
         cmd1 = "env DISPLAY= python DatabaseRatePredictor.py --makeFits "+trigger_string+" --NoVersion "+json_string+" --maxdt=0.10 "+oldMenu
         cmd2 = "sed -i '/TriggerToMonitorList=/ c\TriggerToMonitorList="+triggerlist+"' defaults.cfg"
         cmd3 = "sed -i '/FitFileName=/ c\FitFileName=Fits/2012/Fit_HLT_NoV_10LS_Run"+run1+"to"+run2+".pkl' defaults.cfg"
-        cmd4 = "sed -i '/prettyCSVwriter(/     c\    prettyCSVwriter(\"rateMon_oldmenu.csv\",[80,10,10,10,10,20,20],Header,SortedData,Warn)' DatabaseRateMonitor.py"
+        cmd4 = "sed -i '/prettyCSVwriter(/     c\        prettyCSVwriter(\"rateMon_oldmenu.csv\",[80,10,10,10,10,20,20],Header,core_data,Warn)' DatabaseRateMonitor.py"
         cmd5 = "python DatabaseRateMonitor.py --write --CompareRun="+run2+" --FirstLS="+LS1+" --NumberLS="+nLS1
         cmd6 = "env DISPLAY= python DatabaseRatePredictor.py --makeFits "+trigger_string+" --NoVersion --maxdt=0.10 "+newMenu
         cmd7 = "sed -i '/FitFileName=/ c\FitFileName=Fits/2012/Fit_HLT_NoV_10LS_Run"+run3+"to"+run4+".pkl' defaults.cfg"
-        cmd8 = "sed -i '/prettyCSVwriter(/ c\    prettyCSVwriter(\"rateMon_newmenu.csv\",[80,10,10,10,10,20,20],Header,SortedData,Warn)' DatabaseRateMonitor.py"
+        cmd8 = "sed -i '/prettyCSVwriter(/     c\        prettyCSVwriter(\"rateMon_newmenu.csv\",[80,10,10,10,10,20,20],Header,core_data,Warn)' DatabaseRateMonitor.py"
         cmd9 = "python DatabaseRateMonitor.py --write --CompareRun="+run4+" --FirstLS="+LS2+" --NumberLS="+nLS2
         cmds = [cmd1,cmd2,cmd3,cmd4,cmd5,cmd6,cmd7,cmd8,cmd9]
         for cmd in cmds:
@@ -108,6 +109,7 @@ def main():
 def StripVersion(name):
     if re.match('.*_v[0-9]+',name):
         name = name[:name.rfind('_')]
+        name = string.strip(name)
     return name
 
 def StripPm(name1):
@@ -140,12 +142,12 @@ def compareFiles(new,old,tmd,gdoct,scaler,save_file):
 
     for row1 in r1:
         for row2 in list2:
-            if StripVersion(str(row1[0])) == StripVersion(str(row2[0])):
+            if StripVersion(str(row1[0])) == StripVersion(str(row2[0])):#new and old menu trigs match
                 for row3 in list3:
-                    if StripVersion(str(row3[0])) == StripVersion(str(row2[0])):
+                    if StripVersion(str(row3[0])) == StripVersion(str(row2[0])):#TMD and old menu trigs match
                         row_write = False
                         for row4 in list4:
-                            if StripVersion(str(row4[0])) == StripVersion(str(row3[0])):
+                            if (StripVersion(str(row4[0])) == StripVersion(str(row3[0]))) and not row_write:
                                 w1.writerow([str(row1[0]),str(float(row2[1])*scaler),str(float(row2[2])*scaler),str(row2[5]),str(float(row1[1])*scaler),str(float(row1[2])*scaler),str(row1[5]),str(row4[3]),str(row3[4]),str(row4[32])])
                                 row_write = True
                                 break
