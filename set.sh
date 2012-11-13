016f4211df88f9906f6e592e449bb64de415ca18
Andrea Bocci <Andrea.Bocci@cern.chfwyzard@gmail.com>
2012-11-13 01:14:58 +0000

    clean up

--- set.sh
+++ set.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#! /bin/bash
 export SCRAM_ARCH=slc5_amd64_gcc462
-pushd /nfshome0/cmssw2
-source cmsset_default.sh
-cd /nfshome0/cmssw2/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_4/src
-##cd slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_2/
-#cd slc5_amd64_gcc434/cms/cmssw/CMSSW_4_4_2/
-##cd slc5_amd64_gcc461/cms/cmssw/CMSSW_5_0_1/
+export VO_CMS_SW_DIR=/nfshome0/cmssw2
+source $VO_CMS_SW_DIR/cmsset_default.sh
+cd $VO_CMS_SW_DIR/$SCRAM_ARCH/cms/cmssw/CMSSW_5_2_4/src
 cmsenv
-popd
-#source /nfshome0/abrinke1/UserCode/Andrew/setupCVS.sh
+cd -
