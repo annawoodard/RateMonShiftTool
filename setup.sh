5aa8d66b234deba6f1fb6f1eb55938967a2901f4
annawoodard <awoodard@nd.edu>
2012-12-14 11:54:27 +0000

    Cleaning up to get rid of errors, and taking out dependency on Andrew's user area

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-export SCRAM_ARCH=slc5_amd64_gcc461
-pushd /nfshome0/cmssw
-source cmsset_default.sh
-##cd slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_2/
-##cd slc5_amd64_gcc434/cms/cmssw/CMSSW_4_4_2/
-cd slc5_amd64_gcc461/cms/cmssw/CMSSW_5_0_1/
+export SCRAM_ARCH=slc5_amd64_gcc462
+export VO_CMS_SW_DIR=/nfshome0/cmssw2
+source $VO_CMS_SW_DIR/cmsset_default.sh
+cd $VO_CMS_SW_DIR/$SCRAM_ARCH/cms/cmssw/CMSSW_5_2_4/src
 cmsenv
-popd
-source /nfshome0/abrinke1/UserCode/Andrew/setupCVS.sh
+cd -
+echo "Setting up CMSSW CVS environment"
+export CVS_RSH="ssh"
+export CVSROOT=":gserver:cmssw.cvs.cern.ch:/cvs/CMSSW"
+echo "Getting certificate for CERN kerberos for $USER"
+kinit "$USER@CERN.CH"
+
